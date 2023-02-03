4ac385863142eeb4dc45cb8fb971a861711480a0
ibalarde <123211837+ibalarde@users.noreply.github.com>
2023-02-02 19:23:10 -0500

    Update serverSetup.sh
    removed redundant comments

--- serverSetup.sh
+++ serverSetup.sh
@@ -4,18 +4,15 @@
 #This is a script to install flask and gunicorn for a web application
 #This script needs no input except where stated.
 
+##### CONFIG #####
+
 ##### INSTALL PATH #####
 installPath=
 
 ##### FIREWALL PORT #####
-#This variable can be changed to the port needed to run the server
-#8000 for gunicorn's default, and 443 for SSL connections
 serverPort=8000
 
-############# AFTER INSTALLATION #############
-#To run gunicorn as the service account, use sudo -u sv_gunicorn gunicorn [args] in the directory your application is in
-#To make sure that any changes made to the source update immediately, use the --reload flag
-#SSL can be used with gunicorn by starting it with the flags, --certfile=[pathToCert], --keyfile=[pathToKey], and --bind=443 (to listen for https)
+##### END CONFIG #####
 
 #update package cache
 sudo apt-get update
